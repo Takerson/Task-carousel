863bc9eb14f0596ce57e7ec5dd25d163e6fd7f15
Takerson <98821045+Takerson@users.noreply.github.com>
2022-08-09 13:31:22 +0600

    task 1, module 7

--- src/main/java/com/epam/rd/autotasks/CompleteByRequestTask.java
+++ src/main/java/com/epam/rd/autotasks/CompleteByRequestTask.java
@@ -1,18 +1,37 @@
 package com.epam.rd.autotasks;
 
 public class CompleteByRequestTask implements Task {
+    private boolean isCompleted = false;
+    private boolean isExecuted = false;
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException();
+        if (isCompleted) isExecuted = true;
     }
 
     @Override
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        return isExecuted;
     }
 
     public void complete() {
-        throw new UnsupportedOperationException();
+        isCompleted = true;
     }
 }
+
+//public class CompleteByRequestTask implements Task {
+//
+//    @Override
+//    public void execute() {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    @Override
+//    public boolean isFinished() {
+//        throw new UnsupportedOperationException();
+//    }
+//
+//    public void complete() {
+//        throw new UnsupportedOperationException();
+//    }
+//}
